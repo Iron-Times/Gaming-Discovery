de0ce719fd7feabc59093c3aca29fd2a4aa7ae0f
Steven Cooper <stevengcooper76@gmail.com>
2016-02-27 13:04:44 -0500

    Streams are sorted by the top ten viewer counts. Route adjusted for stream.

--- app/controllers/twitch_controller.rb
+++ app/controllers/twitch_controller.rb
@@ -2,11 +2,18 @@ require 'kappa'
 class TwitchController < ApplicationController
   def streams
     streamers = {}
+    array = []
     v = Twitch.streams.find(:game => params["game"])
     v.each do |stream|
-      next if stream.viewer_count < 1000
-       streamers[stream.user] = stream.url
-    end
+      next if stream.viewer_count < 5
+        array << stream
+        array.sort_by {|stream| stream.viewer_count}
+        array
+      end
+        array.each_with_index do |stream, i|
+          break if i == 10
+          streamers[stream.user] = stream.url
+        end
     render json: streamers
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
 
-  get 'twitch/streams'
+  get 'api/v1/streams' => 'twitch#streams'
 
-  
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
